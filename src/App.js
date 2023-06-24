4a6b64dbadd6334f1d22c5baa98a5e2d68d89a32
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-06-24 17:07:13 +0300

    react hooks and event handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
@@ -9,17 +10,33 @@ const photos = [
   "https://picsum.photos/id/1004/200/200",
   "https://picsum.photos/id/1005/200/200",
   "https://picsum.photos/id/1006/200/200",
-  "https://picsum.photos/200/300",
+  "https://picsum.photos/id/237/200/200",
+  "https://picsum.photos/200/200",
 ];
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => collapse(!isCollapsed);
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
+        <button
+          className="btn btn-warning mx-3"
+          onClick={() =>
+            setItems(["https://picsum.photos/seed/picsum/200/200", ...items])
+          }
+        >
+          +Add
+        </button>
+        <button className="btn btn-success" onClick={() => toggle}>
+          collapse
+        </button>
         <h1>Gallery</h1>
         <div className="row">
-          {photos.map((photo) => (
+          {items.map((photo) => (
             <Card key={photo} src={photo} />
           ))}
         </div>
